56a0ff46aea28f1065f1f19e5f6e76aca91b1557
Areeb Zeeshan <areebzeeshan03@gmail.com>
2023-12-24 17:38:58 +0500

    update

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const githubData = {
+    "message": "Not Found",
+    "documentation_url": "https://docs.github.com/rest",
+};
+
 app.get('/', (req, res) => {
     res.send('Hello world')
 });
@@ -14,6 +19,10 @@ app.get('/tutorial', (req, res) => {
 app.get('/twitter', (req, res) => {
     res.send('<h1>I am at twitter</h1>')
 })
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app is listening on ${process.env.PORT}`)
